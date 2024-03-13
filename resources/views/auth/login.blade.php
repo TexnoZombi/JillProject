50c63344fcd57f2ac2c338d0ccd3b004f0b5f5fd
Vitalya-kon <vitalya.kon123@gmail.com>
2024-03-13 21:26:25 +0500

    Добавил в админке добавление , удаление , редактиирование и т.д для категории

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    {{-- <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
@@ -42,6 +42,65 @@
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
+        </div>
+    </form> --}}
+
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <div class="bg-white px-10 py-8 rounded-xl w-screen shadow-xl max-w-sm">
+            <div class="space-y-4">
+                <h1 class="text-center text-2xl font-semibold text-gray-600">Вход</h1>
+                <hr>
+            <div class="flex items-center border-2 py-2 px-3 rounded-md mb-4">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                </svg>
+
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input id="email" class="pl-2 outline-none border-none w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+            <div class="flex items-center border-2 py-2 px-3 rounded-md">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                </svg>
+                <x-input-label for="password" :value="__('Password')" />
+
+                <x-text-input id="password" class="pl-2 outline-none border-none w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password" />
+
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+            </div>
+            <!-- Remember Me checkbox -->
+            <div class="flex justify-center items-center mt-4">
+                <p class="inline-flex items-center text-gray-700 font-medium text-xs text-center">
+                    <label for="remember_me" class="">
+                        <input id="remember_me" type="checkbox" class="mr-2" name="remember">
+                        <span class="text-xs font-semibold">{{ __('Запомнить меня') }}</span>
+                    </label>
+                </p>
+            </div>
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="mt-6 w-full shadow-xl bg-gradient-to-tr from-blue-600 to-red-400 hover:to-red-700 text-indigo-100 py-2 rounded-md text-lg tracking-wide transition duration-1000">
+                {{ __('Вход') }}
+            </x-primary-button>
+            <hr>
+            <div class="flex justify-center items-center mt-4">
+                <p class="inline-flex items-center text-gray-700 font-medium text-xs text-center">
+                    <span class="ml-2">You don't have an account?<a href="#" class="text-xs ml-2 text-blue-500 font-semibold">Register now &rarr;</a>
+                    </span>
+                </p>
+            </div>
         </div>
     </form>
 </x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\AdminController;
+
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,14 +16,22 @@ use Illuminate\Support\Facades\Route;
 */
 // Route::get('/admin', [AdminController::class, 'index']);
 
-Route::get('/admin', function () {
-    return view('admin');
-});
+Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::group(['prefix' => 'categories'], function () {
+    Route::get('/', \App\Http\Controllers\Category\IndexController::class)->name('category.index');
+    Route::get('/create', \App\Http\Controllers\Category\CreateController::class)->name('category.create');
+    Route::post('/', \App\Http\Controllers\Category\StoreController::class)->name('category.store');
+    Route::get('/{category}/edit', \App\Http\Controllers\Category\EditController::class)->name('category.edit');
+    Route::get('/{category}', \App\Http\Controllers\Category\ShowController::class)->name('category.show');
+    Route::patch('/{category}', \App\Http\Controllers\Category\UpdateController::class)->name('category.update');
+    Route::delete('/{category}', \App\Http\Controllers\Category\DeleteController::class)->name('category.delete');
 
+
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
